98ac546d312b6765787fa5da07103409c0167f36
MIScreanty <redhatawka@gmail.com>
2024-10-17 12:27:51 +0300

    добавили возможность ввести тэг

--- GetCats.py
+++ GetCats.py
@@ -12,7 +12,9 @@ def get_cat(url):
     return img_tk
 
 def get_new_cat():
-    img = get_cat(url)
+    tag = e.get()
+    new_url_tag = f"https://cataas.com/cat/{tag}" if tag else f"https://cataas.com/cat"
+    img = get_cat(new_url_tag)
     if img:
         new_win = Toplevel()
         t_m = Label(new_win, image=img)
@@ -29,8 +31,6 @@ window.title("Caats")
 window.geometry(f"500x440+{window.winfo_screenwidth()//2-250}+{window.winfo_screenheight()//2-220}")
 window.iconbitmap("cat_icon_138789.ico")
 
-url = "https://cataas.com/cat"
-
 #исправим код чтобы получать котов в новом окне
 #t_m = Label(window)
 #t_m.pack()
@@ -38,6 +38,10 @@ url = "https://cataas.com/cat"
 #сделаем меню вместо кнопки
 #btn = Button(window, text="Get new cat", command=get_new_cat)
 #btn.pack()
+
+#ввод тэга
+e = Entry()
+e.pack()
 
 #Создаем меню
 main_menu = Menu(window)
