4e9ea1aec13b85e1b9bca1c1f0a3f33e582a9d87
MIScreanty <redhatawka@gmail.com>
2024-10-17 12:12:32 +0300

    создали функцию для кнопки btn чтобы получать картинку котика при каждом нажатии на кнопку

--- GetCats.py
+++ GetCats.py
@@ -11,18 +11,24 @@ def get_cat(url):
     img_tk = ImageTk.PhotoImage(img)
     return img_tk
 
+def get_new_cat():
+    img = get_cat(url)
+    if img:
+        t_m.config(image=img)
+        t_m.image = img
+
+
 window = Tk()
 window.title("Caats")
-window.geometry(f"500x400+{window.winfo_screenwidth()//2-250}+{window.winfo_screenheight()//2-200}")
+window.geometry(f"500x440+{window.winfo_screenwidth()//2-250}+{window.winfo_screenheight()//2-220}")
 window.iconbitmap("cat_icon_138789.ico")
 
 url = "https://cataas.com/cat"
-img = get_cat(url)
 
-t_m = Label(window, image=img)
+t_m = Label(window)
 t_m.pack()
 
-btn = Button(window, text="Get new cat")
+btn = Button(window, text="Get new cat", command=get_new_cat)
 btn.pack()
 
 window.mainloop()
